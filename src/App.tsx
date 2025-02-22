5d79a659ee99965c6ad2e4e4a434ce0dfe30c4db
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2025-02-22 20:36:05 +0100

    Update styling

--- src/App.tsx
+++ src/App.tsx
@@ -84,7 +84,7 @@ function App() {
             🤔 No countries found with matching criteria.
           </p>
         )}
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-17 pb-10">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:gap-17 lg:grid-cols-3 xl:grid-cols-4 gap-10 pb-10">
           {isLoading && <p>Loading...</p>}
           {!isLoading &&
             filteredCountries.map((country: Country) => (
