6372e5058345dfef2d8171612e203054eb86344a
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2025-02-26 22:22:21 +0100

    Update SearchField.tsx

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -3,13 +3,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function SearchField({ searchString, setSearchString }: any) {
   return (
-    <div className="bg-white flex shrink w-full sm:max-w-[450px] items-center gap-6  px-8 shadow-md rounded-md">
+    <div className="bg-white flex shrink w-full sm:max-w-[450px] items-center gap-6 px-8 shadow-md rounded-md">
       <FontAwesomeIcon icon={faMagnifyingGlass} color="#666" />
       <input
         type="text"
         value={searchString}
         onChange={(e) => setSearchString(e.target.value)}
-        className="py-[18px] border-none outline-none text-gray-600 text-sm w-full"
+        className="py-[16px] border-none outline-none text-zinc-700 w-full"
         placeholder="Search for a country..."
       />
     </div>
