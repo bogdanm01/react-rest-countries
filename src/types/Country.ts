d9fb416ad5d87d9dd892b59eb21bcba1f3bfee52
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2025-02-26 22:23:31 +0100

    Update Country type

--- src/types/Country.ts
+++ src/types/Country.ts
@@ -2,12 +2,19 @@ export type Country = {
   name: {
     common: string;
     official: string;
+    nativeName?: any;
   };
   population: number;
   flags: {
     png: string;
+    svg: string;
   };
   capital: string;
   region: string;
   cca3: string;
+  languages?: any;
+  subregion?: string;
+  tld?: string[];
+  currencies?: any;
+  borders?: string[];
 };
