139c308c0b89e335bf8c8053e402036c07516c6b
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2025-03-01 00:40:07 +0100

    Add svg loader

--- src/pages/CountryPage.tsx
+++ src/pages/CountryPage.tsx
@@ -4,10 +4,12 @@ import { FaArrowLeft } from "react-icons/fa6";
 import CountryDetails from "../components/CountryDetails";
 import Chip from "../components/Chip";
 import { useFetchCountry } from "../hooks/useFetchCountry";
+import { useThemeContext } from "../hooks/useThemeContext";
 
 function CountryPage() {
   const { code } = useParams();
   const { country, isLoading } = useFetchCountry(code!);
+  const { theme } = useThemeContext();
 
   const mappedCountry = useMemo(() => {
     const nativeNameObj = country?.name?.nativeName
@@ -42,12 +44,60 @@ function CountryPage() {
 
   return (
     <div className="px-4 max-w-7xl w-full my-0 mx-auto pb-10">
-      <Link to="..">
-        <button className="cursor-pointer inline-flex gap-2 items-center justify-center bg-white dark:bg-dark-blue dark:text-very-light-gray hover:bg-zinc-50 hover:dark:bg-very-dark-blue-elements py-2 px-8 rounded-md shadow-md">
-          <FaArrowLeft />
-          Back
-        </button>
-      </Link>
+      <div className="flex justify-between">
+        <Link to="..">
+          <button className="cursor-pointer inline-flex gap-2 items-center justify-center bg-white dark:bg-dark-blue dark:text-very-light-gray hover:bg-zinc-50 hover:dark:bg-very-dark-blue-elements py-2 px-8 rounded-md shadow-md">
+            <FaArrowLeft />
+            Back
+          </button>
+        </Link>
+        {isLoading && (
+          <svg
+            width="24"
+            height="24"
+            stroke={`${theme === "light" ? "#888" : "#2b3945"}`}
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g>
+              <circle
+                cx="12"
+                cy="12"
+                r="9.5"
+                fill="none"
+                stroke-width="3"
+                stroke-linecap="round"
+              >
+                <animate
+                  attributeName="stroke-dasharray"
+                  dur="1.5s"
+                  calcMode="spline"
+                  values="0 150;42 150;42 150;42 150"
+                  keyTimes="0;0.475;0.95;1"
+                  keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
+                  repeatCount="indefinite"
+                />
+                <animate
+                  attributeName="stroke-dashoffset"
+                  dur="1.5s"
+                  calcMode="spline"
+                  values="0;-16;-59;-59"
+                  keyTimes="0;0.475;0.95;1"
+                  keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
+                  repeatCount="indefinite"
+                />
+              </circle>
+              <animateTransform
+                attributeName="transform"
+                type="rotate"
+                dur="2s"
+                values="0 12 12;360 12 12"
+                repeatCount="indefinite"
+              />
+            </g>
+          </svg>
+        )}
+      </div>
       {!isLoading && (
         <div className="flex-col flex md:flex-row items-center mt-15 gap-5 md:gap-10 lg:gap-0">
           <div className="flex-1/2 animate-fade-in-left">
